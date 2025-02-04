694c195b14aa2ddd6e7f85472c16fcfe8d35bac0
callie carr <piercyj9@gmail.com>
2025-02-04 13:47:17 -0500

    markiplier fucking explodes

--- src/main/java/bluedazzled/mas/blocks/markiplier.java
+++ src/main/java/bluedazzled/mas/blocks/markiplier.java
@@ -1,11 +1,17 @@
 package bluedazzled.mas.blocks;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
 
 public class markiplier extends Block {
     public markiplier() {
@@ -14,5 +20,12 @@ public class markiplier extends Block {
                 .strength(3.5F)
                 .sound(SoundType.METAL));
 
+    }
+    @Override
+    public InteractionResult useWithoutItem(BlockState state, Level level, BlockPos pos, Player player, BlockHitResult result) {
+        if (!level.isClientSide) {
+            level.explode(null, pos.getX(), pos.getY(), pos.getZ(), 10, true, Level.ExplosionInteraction.TNT);
+        }
+        return InteractionResult.SUCCESS;
     }
 }
