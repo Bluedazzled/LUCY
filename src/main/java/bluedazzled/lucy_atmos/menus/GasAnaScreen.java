3d2214834bc538049e4dc03d23eb8fc4687dda9c
callie carr <piercyj9@gmail.com>
2025-02-12 20:02:59 -0500

    initial menu that opens when shift clicking atmos tiles

---
+++ src/main/java/bluedazzled/lucy_atmos/menus/GasAnaScreen.java
@@ -0,0 +1,69 @@
+package bluedazzled.lucy_atmos.menus;
+
+import net.minecraft.client.GameNarrator;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.components.EditBox;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.client.gui.screens.inventory.ContainerScreen;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.Tag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.MenuType;
+import net.minecraft.world.level.block.entity.BlockEntity;
+
+public class GasAnaScreen extends AbstractContainerScreen<GasAnaMenu> {
+
+    private Player player;
+    private BlockEntity blockent;
+
+    public GasAnaScreen(GasAnaMenu menu, Inventory inv, Component title) {
+        super(menu, inv, Component.literal("gasana_screen"));
+
+    }
+    EditBox box;
+    @Override
+    public void init(){
+        super.init();
+        this.leftPos = (this.width - this.imageWidth) / 2;
+        this.topPos = (this.height - this.imageHeight) / 2;
+
+        this.box = new EditBox(Minecraft.getInstance().font.self(),
+                this.leftPos, this.topPos,
+                64, 16,
+                Component.literal(""));
+
+        this.addRenderableWidget(Button.builder(Component.literal("setTemp"), button -> {onButtonPressed();})
+                .bounds(this.leftPos + 64, this.topPos, 64, 16)
+                .build());
+
+        this.addRenderableWidget(box);
+    }
+
+    @Override
+    public void render(GuiGraphics graphics, int mouseX, int mouseY, float partialTick){
+        super.render(graphics, mouseX, mouseY, partialTick);
+    }
+
+    @Override
+    public void renderBg(GuiGraphics graphics, float p1, int p2, int p3) {
+        graphics.blit(
+                RenderType::guiTextured,
+                MENU_BACKGROUND,
+                this.leftPos, this.topPos,
+                0, 0,
+                this.imageWidth, this.imageHeight,
+                256, 256
+        );
+    }
+
+    private void onButtonPressed(){
+        this.minecraft.player.displayClientMessage(Component.literal(this.box.getValue()), false);
+
+    }
+}
